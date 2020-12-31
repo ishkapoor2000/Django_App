1cd2a9ebe2eeab74761f6ca1bd842e76e56d7cf9
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2020-12-31 17:05:33 +0530

    Update views.py

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -11,4 +11,5 @@ def index(request):
 def book_by_id(request, book_id):
 
 	book = Book.objects.get(pk=book_id)
-	return HttpResponse(f"Book: {book.title}, published on {book.pub_date}")
+	#return HttpResponse(f"Book: {book.title}, published on {book.pub_date}")
+	return render(request, 'book_details.html', {'book':book})
