3f276b278caebfe3da27ffed5058715db7debc39
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2020-12-31 16:52:36 +0530

    Update views.py

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Book
+
 def index(request):
 
 	return HttpResponse("Hello, World!")
+
+def book_by_id(request, book_id):
+
+	book = Book.objects.get(pk=book_id)
+	return HttpResponse(f"Book: {book.title}, published on {book.pub_date}")
